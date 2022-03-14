9b2590067c3db5504e9630b43437e377a51902cf
Kayran Chaves <99879259+KVChaves@users.noreply.github.com>
2022-03-14 13:00:47 +0000

    Questão 3 com erro.

--- ListaDeFunções/exf04.c
+++ ListaDeFunções/exf04.c
@@ -23,28 +23,4 @@ int ler3num(int vetf[]){
   printf("Terceiro: ");
   scanf("%d",&vetf[2]);
   
-    }
-
-/*int main(void)
-{
-    float vmain[3];
-    float ler3numeros (float vetor[]);
-
-    ler3numeros (vmain);
-
-    printf("\n%.1f", vmain[0]);
-    printf("\n%.1f", vmain[1]);
-    printf("\n%.1f", vmain[2]);
-
-    return 0;
 }
-
-    float ler3numeros (float vetor[])
-    {
-            printf("Digite um numero: ");
-            scanf("%f", &vetor[0]);
-            printf("Digite um numero: ");
-            scanf("%f", &vetor[1]);
-            printf("Digite um numero: ");
-            scanf("%f", &vetor[2]);
-    }*/
